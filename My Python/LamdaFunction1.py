86ab21abb8849ea77d4e0d6f8ba928e8fe43457b
KrishnaBajgai <krishnabajgaiskrm@gmail.com>
2023-03-22 21:15:49 +0545

    third Commit

---
+++ My Python/LamdaFunction1.py
@@ -0,0 +1,22 @@
+# # def add(y):
+# #     y=y+10
+# #     return(y)
+# # # print(add(5))
+# # # add=lambda y:y+10
+# # # print(add(6))
+
+# # div=lambda a: a/5
+# # # print(div(10))
+
+# mylist1=[1,2,3,4,5]
+# # for x in mylist1:
+# #     print x
+# # # shortlist=lambda num: for n in num
+# # print(shortlist(mylist1))
+# def Sort1(list1):
+#     for x in list1:
+#         return x
+# print(Sort1(mylist1))
+
+mul=lambda x,y,z: x if x<y and x<z else z
+print(mul(4,5, 2))
